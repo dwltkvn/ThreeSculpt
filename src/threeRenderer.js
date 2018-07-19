ecb2b209bced4fdfe79525f38f39a4ee1d265cfe
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2018-07-19 20:48:04 +0200

    add orbit controller and cube outline

--- src/threeRenderer.js
+++ src/threeRenderer.js
@@ -1,10 +1,17 @@
 import React from "react";
 import * as THREE from "three";
+const OrbitControls = require("three-orbit-controls")(THREE);
 
 class ThreeRenderer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.onResize = this.onResize.bind(this);
+  }
+
   componentDidMount() {
-    const width = 100;
-    const height = width;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0x222222);
@@ -15,16 +22,42 @@ class ThreeRenderer extends React.Component {
     renderer.setSize(width, height);
     renderer.setClearColor(0xffffff, 1.0);
 
-    const camera = new THREE.PerspectiveCamera(35, width / height, 0.1, 1000);
-    camera.position.z = 5;
+    const camera = (this.camera = new THREE.PerspectiveCamera(
+      35,
+      width / height,
+      0.1,
+      1000
+    ));
+    /*
+    const camera = (this.camera = new THREE.OrthographicCamera(
+      width / -2,
+      width / 2,
+      height / 2,
+      height / -2,
+      1,
+      1000
+    ));
+    */
+    camera.position.z = 2;
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const controls = (this.controls = new OrbitControls(this.camera));
+
+    const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
     const material = new THREE.MeshLambertMaterial({
       color: 0xff0000
     });
-
     const cube = new THREE.Mesh(geometry, material);
+
+    const outlineMaterial = new THREE.MeshBasicMaterial({
+      color: 0x000000,
+      side: THREE.BackSide
+    });
+    const outlineMesh = new THREE.Mesh(geometry, outlineMaterial);
+    outlineMesh.position.set(cube.position.x, cube.position.y, cube.position.z); // = cube.position;
+    outlineMesh.scale.multiplyScalar(1.05);
+
     scene.add(cube);
+    scene.add(outlineMesh);
 
     const light_p = new THREE.PointLight(0xffffff);
     light_p.position.set(100, 100, 100);
@@ -37,21 +70,31 @@ class ThreeRenderer extends React.Component {
       requestAnimationFrame(animate);
 
       const f1 = Date.now() / 1000;
-      cube.rotation.x = Math.sin(f1);
-      cube.rotation.y = Math.cos(f1);
-
+      //cube.rotation.x = Math.sin(f1);
+      //cube.rotation.y = Math.cos(f1);
+      controls.update();
       renderer.render(scene, camera);
     };
-
     animate();
 
     this.cube = cube;
+
+    this.onResize();
+    window.addEventListener("resize", this.onResize);
   }
 
   componentWillUnmount() {
+    window.removeEventListener("resize", this.onResize);
     this.renderer.dispose();
   }
 
+  onResize(e) {
+    console.log(e);
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
   storeRef = node => {
     this.canvas = node;
   };
